45c38c3fe7d2cce25669bac1d85bc67b43c922e6
Daniil Sydorenko <daniildeveloper@gmail.com>
2022-07-02 16:18:00 +0200

    add github actions workflow for container

--- packages/container/src/components/Marketing.js
+++ packages/container/src/components/Marketing.js
@@ -3,9 +3,8 @@ import { mount } from 'marketing/Marketing';
 
 export default () => {
     const ref = useRef(null);
-    useEffect(() => {
-        mount(ref.current);
-    }, []);
+
+    useEffect(() => { mount(ref.current); }, []);
 
     return <div ref={ref} />;
 };
