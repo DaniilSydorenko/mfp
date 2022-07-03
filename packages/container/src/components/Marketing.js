dcb81bf2f1c88b27d06677afbf866dfb9ddacd4f
Daniil Sydorenko <daniildeveloper@gmail.com>
2022-07-03 15:58:45 +0200

    fix default region

--- packages/container/src/components/Marketing.js
+++ packages/container/src/components/Marketing.js
@@ -4,7 +4,9 @@ import { mount } from 'marketing/Marketing';
 export default () => {
     const ref = useRef(null);
 
-    useEffect(() => { mount(ref.current); }, []);
+    useEffect(() => {
+        mount(ref.current);
+    }, []);
 
     return <div ref={ref} />;
 };
